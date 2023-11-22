8574ba9d448921e45891ee88cc12fbdb48e6deb0
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-11-22 10:27:29 -0300

    alterando models e requisicao axios

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,20 @@
+import axios, { Axios } from 'axios'
 import { PRODUCT_LIST_FAIL,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_REQUEST } from '../constants/productConstants'
 
-const listProduct = () => {
-    
+const listProduct = () => async (dispatch) => {
+    try{
+        dispatch({type:PRODUCT_LIST_REQUEST})
+        const {data} = await axios.get("/api/products")
+        dispatch({
+            tyoe:PRODUCT_LIST_SUCCESS,
+            process:data
+        })
+    }catch(error){
+        dispatch({
+            tyoe:PRODUCT_LIST_FAIL,
+            payload:error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        })
+    }
 }
